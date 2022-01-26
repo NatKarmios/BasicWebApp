9989ae323a97a59d489301475eb4050084d363ca
Nat Karmios <nat@karmios.com>
2022-01-26 18:33:40 +0000

    Allow returning multiple results

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,17 +1,20 @@
 package com.develogical;
 
+import java.util.ArrayList;
+
 public class QueryProcessor {
 
     public String process(String query) {
+        ArrayList<String> results = new ArrayList<>();
         if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+            results.add("William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
+                    "writer in the English language and the world's pre-eminent dramatist.");
         }
 
         if (query.toLowerCase().contains("imperial")) {
-            return "Imperial College is a university in London";
+            results.add("Imperial College is a university in London");
         }
-        return "";
+        return String.join("<p>", results);
     }
 }
